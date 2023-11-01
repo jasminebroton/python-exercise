ac0e06daf5deb907c5d3469c8da8200f96454e06
Jasmine <jbroton@emich.edu>
2023-11-01 22:06:31 +0000

    feature: added a menu I added a base menu to my movie listing program
    issue #1

--- query.py
+++ query.py
@@ -1,3 +1,10 @@
 from movies import Movies
 
 movies = Movies('./movies.txt')
+
+while True:
+    user_input = input("Menu: \nq: quit")
+    if user_input == 'q':
+        break
+    else:
+        print('Invalid input, try again!')
