b667d3baf93019d9edeee33748c97c92e756bdef
Jasmine <jbroton@emich.edu>
2023-11-02 00:26:06 +0000

    feature: adding the ability to print
    issue #3

--- query.py
+++ query.py
@@ -3,8 +3,10 @@ from movies import Movies
 movies = Movies('./movies.txt')
 
 while True:
-    user_input = input("Menu: \nq: quit")
+    user_input = input("Menu \nq: quit\nlist: print out all movie names")
     if user_input == 'q':
         break
+    elif user_input == 'list':
+        movies.prints()
     else:
         print('Invalid input, try again!')
