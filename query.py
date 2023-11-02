d4245028ac395539d6f0f6bcc74a761201751f08
Jasmine <jbroton@emich.edu>
2023-11-02 00:56:33 +0000

    feature: add the ability to search movies from searchName
    issue #5

--- query.py
+++ query.py
@@ -3,10 +3,13 @@ from movies import Movies
 movies = Movies('./movies.txt')
 
 while True:
-    user_input = input("Menu \nq: quit\nlist: print out all movie names")
+    user_input = input("Menu \nq: quit\nlist: print out all movie names\nsn: search movie names\n")
     if user_input == 'q':
         break
     elif user_input == 'list':
         movies.prints()
+    elif user_input == 'sn':
+        name_input = input("What movie would you like to search?")
+        movies.search(name_input)
     else:
         print('Invalid input, try again!')
